091eaeff3b8e8d002d7c5888731c7264c008a44c
Alexandru Pavaloi <alexandru.pavaloi@tss-yonder.com>
2016-10-21 21:53:10 +0300

    Changed naming of button and quality of picturegit add --all

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera } from 'ionic-native';
 import { ChuckNorrisJokesService } from '../../services/chuck-noris-jokes-service';
 import { ResultPage } from '../result/result';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'page-home',
@@ -10,7 +11,7 @@ import { ResultPage } from '../result/result';
 })
 export class HomePage {
   advice: string;
-  constructor(public navCtrl: NavController, public jokesService: ChuckNorrisJokesService, private elRef: ElementRef) {
+  constructor(public navCtrl: NavController, public jokesService: ChuckNorrisJokesService) {
     this.advice = "";
   }
   getNewJoke() {
@@ -24,13 +25,14 @@ export class HomePage {
   }
 
   getPicture() {
-    Camera.getPicture({ encodingType: Camera.EncodingType.PNG, sourceType: 1, destinationType: Camera.DestinationType.DATA_URL, quality: 1}).then((imageData) => {
+    Camera.getPicture({ encodingType: Camera.EncodingType.JPEG, sourceType: 1, destinationType: Camera.DestinationType.DATA_URL, quality: 30 }).then((imageData) => {
 
-      let base64Image = 'data:image/png;base64,' + imageData;
-      setTimeout(() => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      let timer = Observable.timer(10);
+      timer.subscribe(t => {
         this.navCtrl.push(ResultPage, { base64: base64Image, initialText: this.advice });
 
-      }, 1000);
+      });
     }, (err) => {
       throw new Error("Camera error!")
     });
