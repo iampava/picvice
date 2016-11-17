9640b3eae2c8f68d93d4d1a6ebc08e0e0ba50958
Alexandru Pavaloi <alexandru.pavaloi@tss-yonder.com>
2016-11-17 18:33:46 +0200

    Updated to RC.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { Push, PushToken } from '@ionic/cloud-angular';
 
 
 @Component({
@@ -11,11 +12,23 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   homePage = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, public push: Push) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
       StatusBar.styleDefault();
     });
+
+    this.push.register().then((t: PushToken) => {
+      return this.push.saveToken(t);
+    }).then((t: PushToken) => {
+      console.log('Token saved:', t.token);
+    });
+
+    this.push.rx.notification()
+      .subscribe((msg) => {
+        alert(msg.title + ': ' + msg.text);
+      });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,28 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import {ResultPage} from '../pages/result/result';
+import { ResultPage } from '../pages/result/result';
 import { ChuckNorrisJokesService } from '../services/chuck-noris-jokes-service';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '128221e6'
+  },
+  'push': {
+    'sender_id': '484083244506',
+    'pluginConfig': {
+      'ios': {
+        'badge': true,
+        'sound': true
+      },
+      'android': {
+        'iconColor': '#343434'
+      }
+    }
+  }
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +31,8 @@ import { ChuckNorrisJokesService } from '../services/chuck-noris-jokes-service';
     ResultPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
